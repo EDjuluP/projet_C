e2e4231774a33e85883777f4b7f2591407db334c
EDjuluP <113523133+EDjuluP@users.noreply.github.com>
2023-01-27 16:49:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,20 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #include "fonctions.h"
 
 int main()
 {
-	Joueurs * tabJoueurs = NULL; // Tableau des joueurs
+	Joueurs * TabJoueurs = NULL; // Tableau des joueurs
 
 	printf("============= Bienvenue au jeu des 10000 =============\n");
 
 	printf("\n1 - Commencer une partie !");
-	printf("\n2 - Regarder les regles !\n");
+	printf("\n2 - Afficher les regles !\n");
 
 	// Menu d'acceuil
 	int nombreSaisi;
@@ -32,7 +31,8 @@ int main()
 			system("CLS");
 
 			// Fonction qui va gérer le jeu
-			Jeu(&tabJoueurs);
+			Jeu(&TabJoueurs);
+
 		}
 		else if (nombreSaisi == 2) // On va afficher les règles
 		{
@@ -42,20 +42,21 @@ int main()
 		}
 
 		nombreChoisi = NULL; // On remet à Null
-
+	
 		printf("\n\nVeux-tu rejouer ou quitter ? (1 ou 0) : ");
 		scanf("%d", &nombreChoisi);
 
-		if (nombreChoisi == 1) // Si le joueur veux rejouer
-		{
-			free(tabJoueurs); // On libere la mémoire
-		}
-		else if (nombreChoisi == 0)
+
+		if (nombreChoisi == 0)
 		{
 			printf("\n\n Merci ! A plus !");
 		}
 
 	} while (nombreChoisi != 0); // Vérification si fin du jeu
+
+	int calculPoints(TabJoueurs);
+
+
 
 	return 0;
 }
