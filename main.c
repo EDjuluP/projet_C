fba717d31cca6ae11a4b3c57c1a3d833b3f5b623
EDjuluP <113523133+EDjuluP@users.noreply.github.com>
2023-02-10 08:53:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
-// ce fichier regroupe des fonctions permettant l'execution du jeu des 10000
+// Par Emmanuel DJULU PENGHE et Junior NGANAMODEÏ
+// BTS SN1 - Lycée La Providence (Amiens)
+
+
 
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -8,7 +11,9 @@
 
 #include "fonctions.h"
 
-int main()
+
+
+int main(int argc, char ** argv)
 {
 	Joueurs * TabJoueurs = NULL; // Tableau des joueurs
 
@@ -30,8 +35,10 @@ int main()
 		{
 			system("CLS");
 
+			int nbJoueurs = saisieJoueurs(&TabJoueurs);
+
 			// Fonction qui va gérer le jeu
-			Jeu(&TabJoueurs);
+			Jeu(&TabJoueurs, nbJoueurs);
 
 		}
 		else if (nombreSaisi == 2) // On va afficher les règles
@@ -39,12 +46,50 @@ int main()
 			system("CLS");
 
 			// On va ensuite afficher les règles
+
+			printf("Voici les regles du jeu des 10000 !\n\n");
+
+			// Nombre de joueurs min - max
+			printf("Nombre de des : 6\n\n");
+
+			// Nombre de joueurs min - max
+			printf("Nombre de joueurs : de 2 a 6 \n\n");
+
+			// Regle et but du jeu
+			printf("Regles du jeu :\n\n");
+			printf("Lorsque un joueur atteint ou depasse 10 000 points, il est considere comme\n");
+			printf("temporairement gagnant mais on finit le tour pour permettre a un autre joueur de\n");
+			printf("surpasser ce score.\n\n");
+
+			printf("On lance les des d'un coup. On additionne les points de chaque lancer.\n\n");
+
+			printf("Si on ne fait aucun point lors d un lancer des des, on ne marque aucun point, on\n");
+			printf("perd meme 1000 points a 6 des. Cependant, on ne peut devenir negatif et on\n");
+			printf("descend a 0.\n\n");
+
+			// Barème (Combinaisons)
+			printf("Bareme :\n\n");
+
+			printf("   Combinaison   |  Nombre de points\n");
+			printf("ou valeur de de  |\n");
+			printf("	1	 |	100\n");
+			printf("	5	 |	50\n");
+			printf("      3 des	 |  100 * la valeur du de\n");
+			printf("    identiques	 |\n");
+			printf("      1+1+1	 |  1000\n");
+			printf("      6 des	 |  1000 * la valeur du de\n");
+			printf("    identiques	 |\n");
+			printf("   1+1+1+1+1+1	 |  Partie gagnee\n");
+
+			Sleep(3000);
 		}
 
 		nombreChoisi = NULL; // On remet à Null
 	
-		printf("\n\nVeux-tu rejouer ou quitter ? (1 ou 0) : ");
+		printf("\n\nVeux-tu rejouer ou quitter ? (Oui = 1 ou  Non = 0) : ");
 		scanf("%d", &nombreChoisi);
+
+
 
 
 		if (nombreChoisi == 0)
@@ -54,7 +99,7 @@ int main()
 
 	} while (nombreChoisi != 0); // Vérification si fin du jeu
 
-	int calculPoints(TabJoueurs);
+	
 
 
 
