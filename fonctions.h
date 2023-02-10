3e1897077cb72ef29f52c8f5a5b33a6964dce743
EDjuluP <113523133+EDjuluP@users.noreply.github.com>
2023-02-10 08:53:43 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,12 +1,16 @@
+// Par Emmanuel DJULU PENGHE et Junior NGANAMODEÏ
+// BTS SN1 - Lycée La Providence (Amiens)
+
+
 typedef struct {
 
 	char nom[20];
-	int nbjoueur;
-	int score;
+	int numJoueur;
+	int points;
 
 }Joueurs;
 
 int saisieJoueurs(Joueurs ** TabJoueurs);
-void Jeu(Joueurs ** TabJoueurs);
-int calculPoints(Joueurs ** TabJoueurs, int * tabDes);
-void AffichageScore(Joueurs * TabJoueurs, int nbJoueur, int winner);
+void Jeu(Joueurs ** TabJoueurs, int nbJoueurs);
+int calculPoints(Joueurs ** TabJoueurs, int * tabJeu);
+void AffichageScore(Joueurs * TabJoueurs, int nbJoueurs, int winner);
